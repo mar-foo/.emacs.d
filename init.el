65f84c7075a70c42b8bd8190aedde05b58d0b675
mar-foo <mario.forzanini@studenti.unimi.it>
2021-05-05 16:33:27 +0200

    Minor changes

--- init.el
+++ init.el
@@ -40,15 +40,25 @@
 (setq show-paren-delay 0)		; Highlight matching parentheses
 (delete-selection-mode 1)		; Replace region when inserting text
 
-(set-face-attribute 'variable-pitch nil ; Variable pitch font
-				   :font "Dejavu Serif"
-				   :height 100)
-(set-face-attribute 'fixed-pitch nil
-					:font "JetBrains Mono"
-					:height 110)
-(set-face-attribute 'default nil
-					:font "JetBrains Mono"
-					:height 100)
+;; Fonts
+(defun mf/set-font-faces ()
+  (set-face-attribute 'variable-pitch nil ; Variable pitch font
+					  :font "Dejavu Serif"
+					  :height 100)
+  (set-face-attribute 'fixed-pitch nil
+					  :font "JetBrains Mono"
+					  :height 110)
+  (set-face-attribute 'default nil
+					  :font "JetBrains Mono"
+					  :height 100))
+
+(if (daemonp)
+	(add-hook 'after-make-frame-functions
+			  (lambda (frame)
+				(with-selected-frame frame		
+				  (mf/set-font-faces)
+				  (message "Loading fonts"))))
+  (mf/set-font-faces))
 
 (if (file-exists-p "/sys/class/power_supply/BAT0/capacity")
 	(display-battery-mode))				; Display battery on laptops
@@ -66,11 +76,11 @@
           (modus-themes-paren-match 'intense-bold)
           (modus-themes-completions 'opinionated)
           :config
-          (load-theme 'modus-vivendi t))
+          (load-theme 'modus-operandi t))
 
 (use-package doom-themes
   :disabled
-  :init (load-theme 'doom-one t))
+  :init (load-theme 'doom-vibrant t))
 
 (use-package ewal
   :init
@@ -98,8 +108,18 @@
     (byte-compile-file .el))
   (load-file .el))
 
+;; Startup time and garbage collection
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (message "Emacs ready in %s with %d garbage collections"
+                     (format "%.2f seconds"
+                             (float-time
+                              (time-subtract after-init-time before-init-time)))
+                     gcs-done)))
+(setq gc-cons-threshold (* 2 1000 1000))
 
-
+;; Server
+(setq initial-buffer-choice (lambda () (get-buffer "*dashboard*")))
 
 
 
@@ -109,7 +129,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("8a379e7ac3a57e64de672dd744d4730b3bdb88ae328e8106f95cd81cbd44e0b6" "2035a16494e06636134de6d572ec47c30e26c3447eafeb6d3a9e8aee73732396" "8f5a7a9a3c510ef9cbb88e600c0b4c53cdcdb502cfe3eb50040b7e13c6f4e78e" "ddff22007104a1317014e48ff3d4911a83771a4ccf57185ccebf7f91339dbfb8" "0f7fa4835d02a927d7d738a0d2d464c38be079913f9d4aba9c97f054e67b8db9" "23b564cfb74d784c73167d7de1b9a067bcca00719f81e46d09ee71a12ef7ee82" "c3957b559cf3606c9a40777c5712671db3c7538e5d5ea9f63eb0729afeac832b" default))
+   '("c4bdbbd52c8e07112d1bfd00fee22bf0f25e727e95623ecb20c4fa098b74c1bd" "0a41da554c41c9169bdaba5745468608706c9046231bbbc0d155af1a12f32271" "8a379e7ac3a57e64de672dd744d4730b3bdb88ae328e8106f95cd81cbd44e0b6" "2035a16494e06636134de6d572ec47c30e26c3447eafeb6d3a9e8aee73732396" "8f5a7a9a3c510ef9cbb88e600c0b4c53cdcdb502cfe3eb50040b7e13c6f4e78e" "ddff22007104a1317014e48ff3d4911a83771a4ccf57185ccebf7f91339dbfb8" "0f7fa4835d02a927d7d738a0d2d464c38be079913f9d4aba9c97f054e67b8db9" "23b564cfb74d784c73167d7de1b9a067bcca00719f81e46d09ee71a12ef7ee82" "c3957b559cf3606c9a40777c5712671db3c7538e5d5ea9f63eb0729afeac832b" default))
  '(fci-rule-color "#BA45A3")
  '(global-emojify-mode t)
  '(jdee-db-active-breakpoint-face-colors (cons "#131033" "#1ea8fc"))
@@ -117,7 +137,7 @@
  '(jdee-db-spec-breakpoint-face-colors (cons "#131033" "#546A90"))
  '(objed-cursor-color "#e61f44")
  '(package-selected-packages
-   '(projectile perspective mentor xml-rpc ewal-doom-themes ewal mu4e-dashboard doom-themes elfeed-dashboard elfeed-org vertico org-edna minimap speed-type vterm 0blayout focus spice-mode modus-vivendi-theme company-box esup minibuffer embark window orderless marginalia almost-mono-themes sexy-monochrome-theme vscdark-theme sublime-themes stimmung-themes quasi-monochrome-theme org-mime ivy-posframe guix elfeed-goodies all-the-icons-dired dired evil-smartparens smartparens magit yasnippet-snippets yasnippet emojify ytdious rotate org-fragtog org-wild-notifier xterm-color with-editor use-package treemacs-evil pkg-info ivy-prescient eshell-git-prompt async))
+   '(modus-themes edit-server projectile perspective mentor xml-rpc ewal-doom-themes ewal mu4e-dashboard doom-themes elfeed-dashboard elfeed-org vertico org-edna minimap speed-type vterm 0blayout focus spice-mode modus-vivendi-theme company-box esup minibuffer embark window orderless marginalia almost-mono-themes sexy-monochrome-theme vscdark-theme sublime-themes stimmung-themes quasi-monochrome-theme org-mime ivy-posframe guix elfeed-goodies all-the-icons-dired dired evil-smartparens smartparens magit yasnippet-snippets yasnippet emojify ytdious rotate org-fragtog org-wild-notifier xterm-color with-editor use-package treemacs-evil pkg-info ivy-prescient eshell-git-prompt async))
  '(rustic-ansi-faces
    ["#0c0a20" "#e61f44" "#a7da1e" "#ffd400" "#1ea8fc" "#ff2afc" "#42c6ff" "#f2f3f7"]))
 (custom-set-faces
--- start-exwm.sh
+++ start-exwm.sh
@@ -2,4 +2,4 @@
 #setxkbmap dvorak
 wal -R &
 sxhkd -c /home/mario/.emacs.d/sxhkdrc &
-exec dbus-launch --exit-with-session emacs-27 -mm --debug-init
+exec dbus-launch --exit-with-session emacsclient -c -e '(load-file "/home/mario/.emacs.d/exwm.el")'
