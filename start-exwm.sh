134f75e7a51f444256d9839cbcdcee3e7c3496c3
mar-foo <mario.forzanini@studenti.unimi.it>
2021-05-11 10:05:12 +0200

    Fix exwm on laptop

--- start-exwm.sh
+++ start-exwm.sh
@@ -3,4 +3,4 @@
 setxkbmap gb
 wal -R &
 sxhkd -c /home/mario/.emacs.d/sxhkdrc &
-exec dbus-launch --exit-with-session emacs -mm --debug-init
+exec dbus-launch --exit-with-session emacs -mm --debug-init --eval='(load-file (expand-file-name "~/.emacs.d/exwm.el"))'
