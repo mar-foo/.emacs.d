d94d43643d19584cb8787f687a6affeffd650fda
Mario Forzanini <mario.forzainin@studenti.unimi.it>
2021-08-30 23:30:50 +0200

    Delete unneeded files

--- start-exwm.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-#setxkbmap dvorak
-setxkbmap gb
-wal -R &
-sxhkd -c /home/mario/.emacs.d/sxhkdrc &
-exec dbus-launch --exit-with-session emacs -mm --debug-init --eval='(load-file (expand-file-name "~/.emacs.d/exwm.el"))'
