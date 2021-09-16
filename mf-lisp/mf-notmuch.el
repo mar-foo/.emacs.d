0a09c2e9d7fa257362fa1e7229a36c099721bad8
Mario Forzanini <mario.forzainin@studenti.unimi.it>
2021-09-16 15:08:16 +0200

    Move all configuration inside the progn, move keybinding definition here

--- mf-lisp/mf-notmuch.el
+++ mf-lisp/mf-notmuch.el
@@ -5,30 +5,31 @@
   (unless
       (fboundp 'notmuch)
 	(autoload #'notmuch "notmuch" nil t))
+  (define-key global-map (kbd "C-. m") #'notmuch)
   (require 'mm-encode)
   (require 'mml-sec)
-  (require 'message))
+  (require 'message)
 
-(setq user-mail-address "mario.forzanini@studenti.unimi.it"
-      user-full-name "Mario Forzanini"
-      send-mail-function 'transport
-      sendmail-program (executable-find "msmtp")
-      mail-specify-envelope-from t
-      mail-envelope-from 'header
-      message-sendmail-envelope-from 'header
-	  notmuch-fcc-dirs "Sent"
-      starttls-use-gnutls nil
-      mm-encrypt-option nil
-      mm-sign-option nil
-      mml-secure-opengpg-encrypt-to-self t
-      mml-secure-openpgp-sign-with-sender t
-      mml-secure-smime-encrypt-to-self t
-      mml-secure-smime-sign-with-sender t
-      mail-signature "----------\nMario Forzanini\nhttp://www.marioforzanini.com\n"
-      message-citation-line-format "On %d-%m-%Y, %R %z, %f wrote:\n"
-      message-citation-line-function 'message-insert-formatted-citation-line
-      message-confirm-send nil
-      message-kill-buffer-on-exit t)
+  (setq user-mail-address "mario.forzanini@studenti.unimi.it"
+		user-full-name "Mario Forzanini"
+		send-mail-function 'transport
+		sendmail-program (executable-find "msmtp")
+		mail-specify-envelope-from t
+		mail-envelope-from 'header
+		message-sendmail-envelope-from 'header
+		notmuch-fcc-dirs "Sent"
+		starttls-use-gnutls nil
+		mm-encrypt-option nil
+		mm-sign-option nil
+		mml-secure-opengpg-encrypt-to-self t
+		mml-secure-openpgp-sign-with-sender t
+		mml-secure-smime-encrypt-to-self t
+		mml-secure-smime-sign-with-sender t
+		mail-signature "----------\nMario Forzanini\nhttp://www.marioforzanini.com\n"
+		message-citation-line-format "On %d-%m-%Y, %R %z, %f wrote:\n"
+		message-citation-line-function 'message-insert-formatted-citation-line
+		message-confirm-send nil
+		message-kill-buffer-on-exit t)
   (setq notmuch-saved-searches
 		`((:name "inbox"
 				 :query "tag:inbox"
@@ -48,7 +49,7 @@
 		notmuch-hello-logo 'nil
 		notmuch-hello-sections '(notmuch-hello-insert-header
 								 notmuch-hello-insert-saved-searches
-								 notmuch-hello-insert-alltags))
+								 notmuch-hello-insert-alltags)))
 
 (provide 'mf-notmuch)
 ;;; mf-notmuch.el ends here
