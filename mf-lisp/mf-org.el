8cca124d9a7cc972b395cc236e9c9dfac4b395eb
Mario Forzanini <mario.forzainin@studenti.unimi.it>
2021-09-22 20:55:43 +0200

    Org: Remove 'eval-after-load' from mf-org.el
    Temporarily accept that org will be loaded at startup

--- mf-lisp/mf-org.el
+++ mf-lisp/mf-org.el
@@ -1,6 +1,5 @@
 ;;; mf-org.el --- My configuration for Org mode and Org Roam -*- lexical-binding: t -*-
 ;;; Code:
-(mf/install ob-go)
 (progn
   (unless
 	  (fboundp #'org-mode)
@@ -11,58 +10,55 @@
   (unless
 	  (fboundp #'org-agenda)
 	(autoload #'org-agenda "org-mode" nil t))
-  (eval-after-load 'org-mode
-	(progn
-	  (setq org-agenda-files
-			'("~/Documents/Personal/agenda.org" "~/Documents/Personal/Notes/20210913174909-teaching.org")
-			org-archive-location "~/Documents/Personal/agenda.org::* Archive"
-			org-agenda-window-setup 'other-window
-			org-agenda-restore-windows-after-quit nil
-			org-agenda-sticky nil
-			org-agenda-skip-archived-trees t
-			org-agenda-start-with-log-mode t
-			org-agenda-include-diary t
-			org-log-done 'time
-			org-log-into-drawer t
-			org-capture-templates
-			'(("a" "Agenda" entry
-			   (file+headline "~/Documents/Personal/agenda.org" "Agenda")
-			   "** TODO %^{Action}\nSCHEDULED:%^T\n" :time-prompt t)
-			  ("t" "Teaching")
-			  ("tt" "Teaching General" entry
-			   (file+headline "~/Documents/Personal/Notes/20210913174909-teaching.org" "Agenda")
-			   "** TODO %^{Action}\n%?")
-			  ("tb" "CBI" entry
-			   (file+headline "~/Documents/Personal/Notes/20210921201618-cbi2021.org" "Agenda")
-			   "** TODO %^{Action}\n%?")
-			  ("tp" "CPA" entry
-			   (file+headline "~/Documents/Personal/Notes/20210921201649-cpa2021.org" "Agenda")
-			   "** TODO %^{Action}\n%?")
-			  ("u" "Uni" entry
-			   (file+headline "~/Documents/Personal/agenda.org" "Uni")
-			   "*** TODO %^{Action}%?"))
-			org-todo-keywords
-			'((sequence "TODO(t)" "WAITING(w)" "|" "DONE(d)" "NO(n)"))
-			org-todo-keyword-faces
-			'(("TODO" . org-todo)
-			  ("WAITING" . (:inherit org-todo :foreground "#b0b0b0"))
-			  ("DONE" . org-done)
-			  ("NO" . error))
-			org-agenda-custom-commands
-			'(("a" "Agenda for current week or day"
-			   ((agenda "")
-				(todo))))
-			org-confirm-babel-evaluate nil)
-	  (org-babel-do-load-languages
-	   'org-babel-load-languages
-	   '((emacs-lisp . t)
-		 (shell . t)
-		 (awk . t)
-		 (go . t)))
-	  ;; I like my display-buffer-alist and would like it to be respected
-	  (defun my-switch-to-buffer-other-window (orig-func &rest args)
-		(apply #'switch-to-buffer-other-window args))
-	  (advice-add 'org-switch-to-buffer-other-window :around #'my-switch-to-buffer-other-window))))
+  (setq org-archive-location "~/Documents/Personal/agenda.org::* Archive"
+		org-agenda-window-setup 'other-window
+		org-agenda-restore-windows-after-quit nil
+		org-agenda-sticky nil
+		org-agenda-skip-archived-trees t
+		org-agenda-start-with-log-mode t
+		org-agenda-include-diary t
+		org-log-done 'time
+		org-log-into-drawer t
+		org-capture-templates
+		'(("a" "Agenda" entry
+		   (file+headline "~/Documents/Personal/agenda.org" "Agenda")
+		   "** TODO %^{Action}\nSCHEDULED: %^t\n%?")
+		  ("t" "Teaching")
+		  ("tt" "Teaching General" entry
+		   (file+headline "~/Documents/Personal/Notes/20210913174909-teaching.org" "Agenda")
+		   "** TODO %^{Action}\n%?\n%a")
+		  ("tb" "CBI" entry
+		   (file+headline "~/Documents/Personal/Notes/20210921201618-cbi2021.org" "Agenda")
+		   "** TODO %^{Action}\n%?\n%a")
+		  ("tp" "CPA" entry
+		   (file+headline "~/Documents/Personal/Notes/20210921201649-cpa2021.org" "Agenda")
+		   "** TODO %^{Action}\n%?\n%a")
+		  ("u" "Uni" entry
+		   (file+headline "~/Documents/Personal/agenda.org" "Uni")
+		   "*** TODO %^{Action}\n%?\n%a"))
+		org-todo-keywords
+		'((sequence "TODO(t)" "WAITING(w)" "|" "DONE(d)" "NO(n)"))
+		org-todo-keyword-faces
+		'(("TODO" . org-todo)
+		  ("WAITING" . (:inherit org-todo :foreground "#b0b0b0"))
+		  ("DONE" . org-done)
+		  ("NO" . error))
+		org-agenda-custom-commands
+		'(("a" "Agenda for current week or day"
+		   ((agenda "")
+			(todo))))
+		org-confirm-babel-evaluate nil)
+  (mf/install ob-go)
+  (org-babel-do-load-languages
+   'org-babel-load-languages
+   '((emacs-lisp . t)
+	 (shell . t)
+	 (awk . t)
+	 (go . t)))
+  ;; I like my display-buffer-alist and would like it to be respected
+  (defun my-switch-to-buffer-other-window (orig-func &rest args)
+	(apply #'switch-to-buffer-other-window args))
+  (advice-add 'org-switch-to-buffer-other-window :around #'my-switch-to-buffer-other-window))
 
 (setq diary-file (file-truename "~/Documents/Personal/diary"))
 
@@ -117,8 +113,7 @@
 	 :templates
 	 '(("t" "Teaching" plain "* Agenda\n** TODO %^{Action}\n%?"
 		:if-new (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n#+filetags: Teaching")
-		:unnarrowed t))))
-  (define-key global-map (kbd "C-. n t") #'mf/org-roam-find-teaching))
+		:unnarrowed t)))))
 
 (provide 'mf-org)
 ;;; mf-org.el ends here
