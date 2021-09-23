b4f4b64e6b8905381798e50e0ba927bfc62a10b0
Mario Forzanini <mario.forzainin@studenti.unimi.it>
2021-09-23 23:11:41 +0200

    MPC: Setup for lazy loading

--- mf-lisp/mf-mpc.el
+++ mf-lisp/mf-mpc.el
@@ -1,35 +1,39 @@
 ;;; mf-mpc.el --- Configuration for mpc.el -*- lexical-binding: t -*-
 ;;; Code:
-(require 'mpc)
-(progn
-  (define-key mpc-tagbrowser-mode-map (kbd "a") #'mpc-playlist-add)
-  (define-key mpc-mode-map (kbd "k") #'mpc-stop)
-  (define-key mpc-mode-map (kbd "n") #'next-line)
-  (define-key mpc-mode-map (kbd "p") #'previous-line)
-  (define-key mpc-mode-map (kbd "SPC") #'mpc-toggle-play)
-  (define-key mpc-mode-map (kbd "+") #'mpc-next)
-  (define-key mpc-mode-map (kbd "-") #'mpc-prev)
-  (defun mf/mpc (orig-func)
-	(interactive
-	 (progn
-	   (if current-prefix-arg
-		   (setq mpc-host (read-string
-						   (format-prompt "MPD host and port" mpc-host)
-						   nil nil mpc-host)))
-	   nil))
-	(with-current-buffer
-		(current-buffer)
-	  (setq-local mpc-previous-window-config
-				  (current-window-configuration)))
-	(let* ((tags mpc-browser-tags)
-		   (win (pop-to-buffer (mpc-songs-buf) nil)))
-	  (unless tags (error "Need at least one entry in `mpc-browser-tags'"))
-	  (while
-		  (progn
-			(pop-to-buffer (mpc-tagbrowser-buf (pop tags)) nil)
-			tags)))
-	(mpc-songs-refresh))
-  (advice-add 'mpc :around #'mf/mpc))
+(unless
+	(fboundp #'mpc)
+  (autoload #'mpc "mpc" nil t))
+(eval-after-load 'mpc
+  '(progn
+	 (message "Loaded mpc")
+	 (define-key mpc-tagbrowser-mode-map (kbd "a") #'mpc-playlist-add)
+	 (define-key mpc-mode-map (kbd "k") #'mpc-stop)
+	 (define-key mpc-mode-map (kbd "n") #'next-line)
+	 (define-key mpc-mode-map (kbd "p") #'previous-line)
+	 (define-key mpc-mode-map (kbd "SPC") #'mpc-toggle-play)
+	 (define-key mpc-mode-map (kbd "+") #'mpc-next)
+	 (define-key mpc-mode-map (kbd "-") #'mpc-prev)
+	 (defun mf/mpc (orig-func)
+	   (interactive
+		(progn
+		  (if current-prefix-arg
+			  (setq mpc-host (read-string
+							  (format-prompt "MPD host and port" mpc-host)
+							  nil nil mpc-host)))
+		  nil))
+	   (with-current-buffer
+		   (current-buffer)
+		 (setq-local mpc-previous-window-config
+					 (current-window-configuration)))
+	   (let* ((tags mpc-browser-tags)
+			  (win (pop-to-buffer (mpc-songs-buf) nil)))
+		 (unless tags (error "Need at least one entry in `mpc-browser-tags'"))
+		 (while
+			 (progn
+			   (pop-to-buffer (mpc-tagbrowser-buf (pop tags)) nil)
+			   tags)))
+	   (mpc-songs-refresh))
+	 (advice-add 'mpc :around #'mf/mpc)))
 
 (provide 'mf-mpc)
 ;;; mf-mpc.el ends here
