f1f8fde740828b817869ecb048c2de78ecc86a68
Mario Forzanini <mario.forzainin@studenti.unimi.it>
2021-09-23 23:11:24 +0200

    ERC: Setup for lazy loading

--- mf-lisp/mf-erc.el
+++ mf-lisp/mf-erc.el
@@ -7,10 +7,13 @@
   (unless
 	  (fboundp #'erc-tls)
 	(autoload #'erc-tls "erc" nil t))
-  (add-hook 'erc-mode-hook #'(lambda() (whitespace-mode -1)))
-  (setq erc-autojoin-channels-alist
-		'(("irc.libera.chat" "#emacs")
-		  ("localhost" "&bitlbee"))))
+  (eval-after-load 'erc
+	'(progn
+	   (message "Loaded erc")
+	   (add-hook 'erc-mode-hook #'(lambda() (whitespace-mode -1)))
+	   (setq erc-autojoin-channels-alist
+			 '(("irc.libera.chat" "#emacs")
+			   ("localhost" "&bitlbee"))))))
 
 (provide 'mf-erc)
 ;;; mf-erc.el ends here
