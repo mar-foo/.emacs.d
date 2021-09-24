eeb2e671edafc6d63010949547093057d3974402
Mario Forzanini <mario.forzainin@studenti.unimi.it>
2021-09-24 23:11:58 +0200

    Mu4e: Add bookmarks to the main page and keybindings

--- mf-lisp/mf-mu4e.el
+++ mf-lisp/mf-mu4e.el
@@ -11,7 +11,6 @@
      (setq user-mail-address "mario.forzanini@studenti.unimi.it"
            user-full-name "Mario Forzanini"
            mu4e-get-mail-command "mbsync -c ~/.mbsyncrc -a mario.forzanini@studenti.unimi.it"
-           mu4e-update-interval 300
            mu4e-compose-signature
            (concat
 			"----------\n"
@@ -26,7 +25,26 @@
            smtpmail-stream-type 'ssl
            mu4e-sent-folder "/Sent"
            mu4e-drafts-folder "/Drafts"
-           mu4e-trash-folder "/Trash")))
+           mu4e-trash-folder "/Trash"
+		   mu4e-headers-date-format "%d/%m/%Y"
+		   mu4e-date-format-long "%d/%m/%Y"
+		   mu4e-change-filenames-when-moving t)
+	 (add-to-list 'mu4e-bookmarks
+				  '(:name "Uni"
+						  :key ?U
+						  :query "from:unimi.it AND to:mario.forzanini@studenti.unimi.it OR from:mario.forzanini@studenti.unimi.it AND to:unimi.it"))
+	 (add-to-list 'mu4e-bookmarks
+				  '(:name "Traccia"
+						  :key ?T
+						  :query "from:latracciabg.it OR to:latracciabg.it"))
+	 (add-to-list 'mu4e-bookmarks
+				  '(:name "9front"
+						  :key ?9
+						  :query "from:9front.org OR to:9front.org"))
+	 (define-key mu4e-main-mode-map (kbd "n") #'next-line)
+	 (define-key mu4e-main-mode-map (kbd "p") #'previous-line)
+	 (define-key mu4e-headers-mode-map (kbd "C-. .") 'mu4e-org-store-and-capture)
+	 (define-key mu4e-view-mode-map (kbd "C-. .") 'mu4e-org-store-and-capture)))
 
 (provide 'mf-mu4e)
 ;;; mf-mu4e.el ends here
