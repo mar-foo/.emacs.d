443b0475f9234137361cbed09fa6707ed48311fc
Mario Forzanini <mario.forzainin@studenti.unimi.it>
2021-09-30 21:27:17 +0200

    Use orderless for completion

--- mf-lisp/mf-completion.el
+++ mf-lisp/mf-completion.el
@@ -1,24 +1,26 @@
 ;;; mf-completion.el --- Configuration for the completion framework -*- lexical-binding: t -*-
-;;; Code:
+  ;;; Code:
 (setq tab-always-indent 'complete)
 (autoload 'ffap-file-at-point "ffap")
 (defun complete-path-at-point+ ()
   "Return completion data for UNIX path at point."
   (let ((fn (ffap-file-at-point))
-        (fap (thing-at-point 'filename)))
-    (when (and (or fn (equal "/" fap))
-               (save-excursion
-                 (search-backward fap (line-beginning-position) t)))
-      (list (match-beginning 0)
-            (match-end 0)
-            #'completion-file-name-table :exclusive 'no))))
+		(fap (thing-at-point 'filename)))
+	(when (and (or fn (equal "/" fap))
+			   (save-excursion
+				 (search-backward fap (line-beginning-position) t)))
+	  (list (match-beginning 0)
+			(match-end 0)
+			#'completion-file-name-table :exclusive 'no))))
 (add-hook 'completion-at-point-functions
-          #'complete-path-at-point+
-          'append)
+		  #'complete-path-at-point+
+		  'append)
 
-(setq completion-styles '(substring partial-completion initials flex)
+(mf/install orderless)
+;; (setq completion-styles '(substring partial-completion initials flex)
+(setq completion-styles '(orderless)
 	  completion-category-overrides
-	  '((file (styles initials basic))))
+	  '((file (styles partial-completion initials basic))))
 
 (mf/install vertico)
 (eval-after-load 'minibuffer
