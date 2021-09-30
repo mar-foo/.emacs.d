66ada222035833f43acde95c19229e265a01a697
Mario Forzanini <mario.forzainin@studenti.unimi.it>
2021-09-30 21:36:32 +0200

    Magit: Move configuration to mf-git.el

---
+++ mf-lisp/mf-git.el
@@ -0,0 +1,15 @@
+;;; mf-git.el --- Git workflow configuration -*- lexical-binding: t -*-
+;;; Code:
+(mf/install magit)
+(progn
+  (mf/autoload-func
+   :func magit
+   :file "magit")
+  (mf/autoload-func
+   :func magit-status
+   :file "magit")
+  (eval-after-load 'magit
+	'(message "Loaded magit")))
+
+(provide 'mf-git)
+;;; mf-git.el ends here
