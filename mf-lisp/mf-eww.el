f24dfa36129cb7d8d19b70283483b047e272f019
Mario Forzanini <mario.forzanini@studenti.unimi.it>
2021-10-10 20:59:25 +0200

    eww: Add mf-eww.el

---
+++ mf-lisp/mf-eww.el
@@ -0,0 +1,6 @@
+;;; mf-eww.el --- Configuration for eww -*- lexical-binding: t -*-
+;;; Code
+(eval-after-load 'eww
+  '(add-to-list 'eww-suggest-uris #'mf/bookmarks))
+(provide 'mf-eww)
+;;; mf-exwm.el ends here
