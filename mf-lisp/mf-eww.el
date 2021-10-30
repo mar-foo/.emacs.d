08c1424374772fda376fa889b00c6cb0fc707990
Mario Forzanini <mario.forzainin@studenti.unimi.it>
2021-10-30 16:34:40 +0200

    Eww: Change buffer name based on page's title

--- mf-lisp/mf-eww.el
+++ mf-lisp/mf-eww.el
@@ -1,6 +1,20 @@
 ;;; mf-eww.el --- Configuration for eww -*- lexical-binding: t -*-
 ;;; Code
 (eval-after-load 'eww
-  '(append eww-suggest-uris (list #'mf/bookmarks)))
+  '(progn
+	 (append eww-suggest-uris (list #'mf/bookmarks))
+	 ;; From protesilaos prot-eww.el https://protesilaos/dotemacs
+	 (defun mf/eww--rename-buffer ()
+	   "Rename EWW buffer using page title or URL.
+To be used by `eww-after-render-hook'."
+	   (let ((name (if (eq "" (plist-get eww-data :title))
+					   (plist-get eww-data :url)
+					 (plist-get eww-data :title))))
+		 (rename-buffer (format "*eww | %s*" name) t)))
+
+(add-hook 'eww-after-render-hook #'mf/eww--rename-buffer)
+(advice-add 'eww-back-url :after #'mf/eww--rename-buffer)
+(advice-add 'eww-forward-url :after #'mf/eww--rename-buffer)))
+
 (provide 'mf-eww)
-;;; mf-exwm.el ends here
+;;; mf-eww.el ends here
