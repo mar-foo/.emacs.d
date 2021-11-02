4dc909faa1faf9f0e0f5454cfe01eb4aeb8035b2
Mario Forzanini <mario.forzanini@studenti.unimi.it>
2021-11-02 15:02:59 +0100

    RSS: Make elfeed respect the display-buffer-alist showing entries

--- mf-lisp/mf-rss.el
+++ mf-lisp/mf-rss.el
@@ -20,12 +20,12 @@
 			 ("https://www.independent.co.uk/news/world/rss" news world)
 			 ;; Italia
 			 ("http://xml2.corriereobjects.it/rss/homepage.xml" news italy)
-			 ("http://www.repubblica.it/rss/homepage/rss2.0.xml" news italy)
 			 ("https://www.ansa.it/sito/ansait_rss.xml" news italy)
 			 ("http://www.governo.it/feed/rss" news italy)
 			 ;; Tech
 			 ("https://www.wired.com/feed/rss" tech)
-			 ("http://rss.slashdot.org/Slashdot/slashdotMain" tech)))
+			 ("http://rss.slashdot.org/Slashdot/slashdotMain" tech))
+		   elfeed-show-entry-switch #'display-buffer)
 	 (setq-default elfeed-search-filter "@1-week-ago +unread")
 	 (add-hook 'elfeed-new-entry-hook
 			   (elfeed-make-tagger :feed-url "youtube\\.com"
