f5f4ad6e8145b1c2a3f18c5f419d25f3e069e9fb
Mario Forzanini <mario.forzanini@studenti.unimi.it>
2021-11-22 23:35:25 +0100

    Telega: move mf/toggle-telega to mf-telega.el

--- mf-lisp/mf-telega.el
+++ mf-lisp/mf-telega.el
@@ -7,6 +7,19 @@
 (setq telega-chat-show-avatars nil
 	  telega-root-show-avatars nil
 	  telega-user-show-avatars nil)
+;;;###autoload
+(defun mf/toggle-telega (&optional use-generic-p)
+  "Toggle telega root buffer, if called with a prefix argument
+	switch to the buffer."
+  (interactive "P")
+  (if (get-buffer-window "*Telega Root*")
+	  (delete-window (get-buffer-window "*Telega Root*"))
+	(if use-generic-p
+		(progn
+		  (telega)
+		  (delete-window (get-buffer-window "*Telega Root*"))
+		  (switch-to-buffer "*Telega Root*"))
+	  (telega))))
 
 (provide 'mf-telega)
 ;;; mf-telega.el ends here
