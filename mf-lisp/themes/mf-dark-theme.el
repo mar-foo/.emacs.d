af3057e0c9aac6e093cbb680f5c0a5dbe6a5603a
Mario Forzanini <mario.forzainin@studenti.unimi.it>
2022-04-02 08:45:16 +0200

    theme: Refactor adding variables for common colors

--- mf-lisp/themes/mf-dark-theme.el
+++ mf-lisp/themes/mf-dark-theme.el
@@ -21,21 +21,33 @@
 (deftheme mf-dark
   "White on black theme for your focused hacking sessions during the night.")
 
+(defvar mf-default-bg
+  "#222222")
+
+(defvar mf-default-fg
+  "white")
+
+(defvar mf-medium-grey
+  "#808080")
+
+(defvar mf-dark-grey
+  "#303030")
+
 (custom-theme-set-faces 'mf-dark
-			'(default ((t (:foreground "white" :background "#1e1e1e"))))
-			'(cursor ((t (:background "white"))))
+			`(default ((t (:foreground ,mf-default-fg :background ,mf-default-bg))))
+			`(cursor ((t (:background ,mf-default-fg))))
 
 			;; Highlighting faces
 			'(bookmark-face ((t (:foreground "black" :background "#cccccc"))))
-			'(fringe ((t (:foreground "white" :background "#1e1e1e"))))
-			'(highlight ((t (:foreground "black" :background "white"))))
-			'(hl-line ((t (:background "#303030"))))
+			`(fringe ((t (:foreground ,mf-default-fg :background ,mf-default-bg))))
+			`(highlight ((t (:foreground "black" :background ,mf-default-fg))))
+			`(hl-line ((t (:background ,mf-dark-grey))))
 			'(isearch ((t (:foreground "#f0f0f0" :background "#006800"))))
-			'(lazy-highlight ((t (:foreground "black" :background "#303030"))))
+			`(lazy-highlight ((t (:foreground "black" :background ,mf-dark-grey))))
 			'(linum ((t (:foreground "#f0f0f0" :slant italic))))
 			'(match ((t (:background "green"))))
-			'(region ((t (:extend t :foreground "black" :background "white"))))
-			'(secondary-selection ((t (:foreground: "white" :background "#303030"))))
+			`(region ((t (:extend t :foreground "black" :background ,mf-medium-grey))))
+			`(secondary-selection ((t (:foreground: ,mf-default-fg :background ,mf-dark-grey))))
 			'(trailing-whitespace ((t (:background "red"))))
 
 			;; Button and link faces
@@ -43,36 +55,36 @@
 			'(link-visited ((t (:underline t :foreground "#bababa"))))
 
 			;; Company
-			'(company-scrollbar-bg ((t (:background "#1e1e1e"))))
-			'(company-scrollbar-fg ((t (:foreground "white"))))
+			`(company-scrollbar-bg ((t (:background ,mf-default-bg))))
+			`(company-scrollbar-fg ((t (:foreground ,mf-default-fg))))
 			'(company-tooltip-annotation ((t (:foreground "#b0b0b0"))))
-			'(company-tooltip-common ((t (:foreground "white" :weight semi-bold))))
-			'(company-tooltip-selection ((t (:foreground "white" :background "#808080"))))
+			`(company-tooltip-common ((t (:foreground ,mf-default-fg :weight semi-bold))))
+			`(company-tooltip-selection ((t (:foreground ,mf-default-fg :background ,mf-medium-grey))))
 
 			;; Corfu
-			'(corfu-current ((t (:foreground "white" :background "#b3b3b3"))))
-			'(corfu-bar ((t (:background "white"))))
-			'(corfu-background ((t (:background "#1e1e1e"))))
+			`(corfu-current ((t (:foreground ,mf-default-fg :background "#b3b3b3"))))
+			`(corfu-bar ((t (:background ,mf-default-fg))))
+			`(corfu-background ((t (:background ,mf-default-bg))))
 			'(corfu-border ((t (:background "#b3b3b3"))))
 
 			;; Erc
-			'(erc-current-nick-face ((t (:foreground "white" :weight bold))))
+			`(erc-current-nick-face ((t (:foreground ,mf-default-fg :weight bold))))
 			'(erc-error-face ((t (:inherit error))))
-			'(erc-keyword-face ((t (:foreground "white" :underline t :weight bold))))
+			`(erc-keyword-face ((t (:foreground ,mf-default-fg :underline t :weight bold))))
 			'(erc-my-nick-face ((t (:inherit erc-current-nick-face))))
 			'(erc-notice-face ((t (:foreground "#b0b0b0" :weight bold))))
-			'(erc-prompt-face ((t (:foreground "white" :underline t :weight bold))))
+			`(erc-prompt-face ((t (:foreground ,mf-default-fg :underline t :weight bold))))
 			'(erc-timestamp-face ((t (:foreground "#b0b0b0" :weight bold))))
 
 			;; Escape and prompt faces
 			'(minibuffer-prompt ((t (:weight bold :foreground "#bababa"))))
 			'(escape-glyph ((t (:foreground "#bababa"))))
 			'(error ((t (:weight bold :slant italic :foreground "#900000"))))
-			'(warning ((t (:foreground "white" :background "#1e1e1e" :inherit font-lock-warning-face))))
+			`(warning ((t (:foreground ,mf-default-fg :background ,mf-default-bg :inherit font-lock-warning-face))))
 			'(success ((t (:foreground "green"))))
 
 			;; EShell
-			'(eshell-prompt ((t (:foreground "white" :bold t))))
+			`(eshell-prompt ((t (:foreground ,mf-default-fg :bold t))))
 			'(eshell-ls-archive ((t (:inherit eshell-ls-unreadable))))
 			'(eshell-ls-backup ((t (:inherit eshell-ls-unreadable))))
 			'(eshell-ls-clutter ((t (:inherit eshell-ls-unreadable))))
@@ -88,11 +100,11 @@
 			'(font-lock-builtin-face ((t (:foreground "#bababa"))))
 			'(font-lock-comment-face ((t (:slant italic :foreground "#b3b3b3"))))
 			'(font-lock-constant-face ((t (:weight bold :foreground "#bababa"))))
-			'(font-lock-function-name-face ((t (:foreground "white" :weight semi-bold))))
-			'(font-lock-keyword-face ((t (:weight bold :foreground "white"))))
-			'(font-lock-string-face ((t (:foreground "#808080"))))
+			`(font-lock-function-name-face ((t (:foreground ,mf-default-fg :weight semi-bold))))
+			`(font-lock-keyword-face ((t (:weight bold :foreground ,mf-default-fg))))
+			`(font-lock-string-face ((t (:foreground ,mf-medium-grey))))
 			'(font-lock-type-face ((t (:weight bold :foreground "#bababa"))))
-			'(font-lock-variable-name-face ((t (:weight bold :foreground "#808080" :slant oblique))))
+			`(font-lock-variable-name-face ((t (:weight bold :foreground ,mf-medium-grey :slant oblique))))
 			'(font-lock-warning-face ((t (:foreground "#b0b0b0" :underline (:color foreground-color :style wave)))))
 
 			;; Gnus faces
@@ -121,53 +133,53 @@
 			'(gnus-header-newsgroups ((t (:foreground "#bababa"))))
 
 			;; helm
-			'(helm-header ((t (:foreground "#bababa" :background "#1e1e1e" :underline nil :box nil))))
+			`(helm-header ((t (:foreground "#bababa" :background ,mf-default-bg :underline nil :box nil))))
 			'(helm-source-header
 			  ((t (:foreground "#bababa"
 					   :underline nil
 					   :weight bold
 					   :box (:line-width 1 :style released-button)))))
 			'(helm-selection ((t (:underline t :foreground "#bababa"))))
-			'(helm-visible-mark ((t (:foreground "#bababa" :background "#1e1e1e"))))
-			'(helm-candidate-number ((t (:foreground "#bababa" :background "#1e1e1e"))))
-			'(helm-separator ((t (:foreground "#bababa" :background "#1e1e1e"))))
-			'(helm-time-zone-current ((t (:foreground "#bababa" :background "#1e1e1e"))))
-			'(helm-time-zone-home ((t (:foreground "#bababa" :background "#1e1e1e"))))
-			'(helm-bookmark-addressbook ((t (:foreground "#bababa" :background "#1e1e1e"))))
+			`(helm-visible-mark ((t (:foreground "#bababa" :background ,mf-default-bg))))
+			`(helm-candidate-number ((t (:foreground "#bababa" :background ,mf-default-bg))))
+			`(helm-separator ((t (:foreground "#bababa" :background ,mf-default-bg))))
+			`(helm-time-zone-current ((t (:foreground "#bababa" :background ,mf-default-bg))))
+			`(helm-time-zone-home ((t (:foreground "#bababa" :background ,mf-default-bg))))
+			`(helm-bookmark-addressbook ((t (:foreground "#bababa" :background ,mf-default-bg))))
 			'(helm-bookmark-directory ((t (:foreground nil :background nil :inherit helm-ff-directory))))
 			'(helm-bookmark-file ((t (:foreground nil :background nil :inherit helm-ff-file))))
-			'(helm-bookmark-gnus ((t (:foreground "#bababa" :background "#1e1e1e"))))
-			'(helm-bookmark-info ((t (:foreground "#bababa" :background "#1e1e1e"))))
-			'(helm-bookmark-man ((t (:foreground "#bababa" :background "#1e1e1e"))))
-			'(helm-bookmark-w3m ((t (:foreground "#bababa" :background "#1e1e1e"))))
-			'(helm-buffer-not-saved ((t (:foreground "#bababa" :background "#1e1e1e"))))
-			'(helm-buffer-process ((t (:foreground "#bababa" :background "#1e1e1e"))))
-			'(helm-buffer-saved-out ((t (:foreground "#bababa" :background "#1e1e1e"))))
-			'(helm-buffer-size ((t (:foreground "#bababa" :background "#1e1e1e"))))
-			'(helm-ff-directory ((t (:foreground "#bababa" :background "#1e1e1e" :weight bold))))
-			'(helm-ff-file ((t (:foreground "#bababa" :background "#1e1e1e" :weight normal))))
-			'(helm-ff-executable ((t (:foreground "#bababa" :background "#1e1e1e" :weight normal))))
-			'(helm-ff-invalid-symlink ((t (:foreground "black" :background "#1e1e1e" :weight bold))))
-			'(helm-ff-symlink ((t (:foreground "#bababa" :background "#1e1e1e" :weight bold))))
-			'(helm-ff-prefix ((t (:foreground "#bababa" :background "#1e1e1e" :weight normal))))
-			'(helm-grep-cmd-line ((t (:foreground "#bababa" :background "#1e1e1e"))))
-			'(helm-grep-file ((t (:foreground "#bababa" :background "#1e1e1e"))))
-			'(helm-grep-finish ((t (:foreground "#bababa" :background "#1e1e1e"))))
-			'(helm-grep-lineno ((t (:foreground "#bababa" :background "#1e1e1e"))))
+			`(helm-bookmark-gnus ((t (:foreground "#bababa" :background ,mf-default-bg))))
+			`(helm-bookmark-info ((t (:foreground "#bababa" :background ,mf-default-bg))))
+			`(helm-bookmark-man ((t (:foreground "#bababa" :background ,mf-default-bg))))
+			`(helm-bookmark-w3m ((t (:foreground "#bababa" :background ,mf-default-bg))))
+			`(helm-buffer-not-saved ((t (:foreground "#bababa" :background ,mf-default-bg))))
+			`(helm-buffer-process ((t (:foreground "#bababa" :background ,mf-default-bg))))
+			`(helm-buffer-saved-out ((t (:foreground "#bababa" :background ,mf-default-bg))))
+			`(helm-buffer-size ((t (:foreground "#bababa" :background ,mf-default-bg))))
+			`(helm-ff-directory ((t (:foreground "#bababa" :background ,mf-default-bg :weight bold))))
+			`(helm-ff-file ((t (:foreground "#bababa" :background ,mf-default-bg :weight normal))))
+			`(helm-ff-executable ((t (:foreground "#bababa" :background ,mf-default-bg :weight normal))))
+			`(helm-ff-invalid-symlink ((t (:foreground "black" :background ,mf-default-bg :weight bold))))
+			`(helm-ff-symlink ((t (:foreground "#bababa" :background ,mf-default-bg :weight bold))))
+			`(helm-ff-prefix ((t (:foreground "#bababa" :background ,mf-default-bg :weight normal))))
+			`(helm-grep-cmd-line ((t (:foreground "#bababa" :background ,mf-default-bg))))
+			`(helm-grep-file ((t (:foreground "#bababa" :background ,mf-default-bg))))
+			`(helm-grep-finish ((t (:foreground "#bababa" :background ,mf-default-bg))))
+			`(helm-grep-lineno ((t (:foreground "#bababa" :background ,mf-default-bg))))
 			'(helm-grep-match ((t (:foreground nil :background nil :inherit helm-match))))
-			'(helm-grep-running ((t (:foreground "#bababa" :background "#1e1e1e"))))
-			'(helm-moccur-buffer ((t (:foreground "#bababa" :background "#1e1e1e"))))
-			'(helm-mu-contacts-address-face ((t (:foreground "#bababa" :background "#1e1e1e"))))
-			'(helm-mu-contacts-name-face ((t (:foreground "#bababa" :background "#1e1e1e"))))
+			`(helm-grep-running ((t (:foreground "#bababa" :background ,mf-default-bg))))
+			`(helm-moccur-buffer ((t (:foreground "#bababa" :background ,mf-default-bg))))
+			`(helm-mu-contacts-address-face ((t (:foreground "#bababa" :background ,mf-default-bg))))
+			`(helm-mu-contacts-name-face ((t (:foreground "#bababa" :background ,mf-default-bg))))
 
 			;; ido
 			'(ido-first-match ((t (:foreground "#bababa"))))
 			'(ido-only-match ((t (:underline "#bababa" :foreground "#bababa"))))
-			'(ido-subdir ((t (:weight bold :foreground "white"))))
+			`(ido-subdir ((t (:weight bold :foreground ,mf-default-fg))))
 
 			;; Magit
-			'(magit-branch-remote ((t (:foreground "white" :underline t :slant oblique))))
-			'(magit-section-heading ((t (:extend t :foreground "white" :weight bold))))
+			`(magit-branch-remote ((t (:foreground ,mf-default-fg :underline t :slant oblique))))
+			`(magit-section-heading ((t (:extend t :foreground ,mf-default-fg :weight bold))))
 
 			;; Message faces
 			'(message-header-name ((t (:foreground "#bababa"))))
@@ -184,44 +196,44 @@
 			'(message-separator ((t (:weight bold :foreground "#bababa"))))
 
 			;; Minibuffer
-			'(minibuffer-prompt ((t (:foreground "white" :weight bold))))
-			'(completions-common-part ((t (:foreground "white" :weight bold))))
-			'(completions-first-difference ((t (:underline "white" :weight semi-bold))))
+			`(minibuffer-prompt ((t (:foreground ,mf-default-fg :weight bold))))
+			`(completions-common-part ((t (:foreground ,mf-default-fg :weight bold))))
+			`(completions-first-difference ((t (:underline ,mf-default-fg :weight semi-bold))))
 
 			;; Mode line faces
-			'(mode-line ((t (:background "#1e1e1e" :foreground "white"
-						     :box (:line-width 4 :color "#1e1e1e" :style flat-button)))))
-			'(mode-line-inactive ((t (:background "#1e1e1e" :foreground "#808080"
-							      :box (:line-width 4 :color "black")))))
+			`(mode-line ((t (:background ,mf-default-bg :foreground ,mf-default-fg
+						     :box (:line-width 4 :color ,mf-default-bg :style flat-button)))))
+			`(mode-line-inactive ((t (:background ,mf-default-bg :foreground ,mf-medium-grey
+							      :box (:line-width 4 :color ,mf-default-bg)))))
 
 			;; Mini- modeline
-			'(mini-modeline-mode-line ((t (:height 0.14 :background "white"))))
-			'(mini-modeline-mode-line-inactive ((t (:height 0.14 :background "#1e1e1e"))))
+			`(mini-modeline-mode-line ((t (:height 0.14 :background ,mf-default-fg))))
+			`(mini-modeline-mode-line-inactive ((t (:height 0.14 :background ,mf-default-bg))))
 
 			;; MuMaMo
-			'(mumamo-background-chunk-major ((t (:background "#1e1e1e"))))
-			'(mumamo-background-chunk-submode1 ((t (:background "#1e1e1e"))))
-			'(mumamo-background-chunk-submode2 ((t (:background "#1e1e1e"))))
-			'(mumamo-background-chunk-submode3 ((t (:background "#1e1e1e"))))
-			'(mumamo-background-chunk-submode4 ((t (:background "#1e1e1e"))))
+			`(mumamo-background-chunk-major ((t (:background ,mf-default-bg))))
+			`(mumamo-background-chunk-submode1 ((t (:background ,mf-default-bg))))
+			`(mumamo-background-chunk-submode2 ((t (:background ,mf-default-bg))))
+			`(mumamo-background-chunk-submode3 ((t (:background ,mf-default-bg))))
+			`(mumamo-background-chunk-submode4 ((t (:background ,mf-default-bg))))
 			'(mumamo-border-face-in ((t (:slant unspecified :underline unspecified
-							    :weight bold :foreground "white"))))
+							    :weight bold :foreground ,mf-default-fg))))
 			'(mumamo-border-face-out ((t (:slant unspecified :underline unspecified
-							     :weight bold :foreground "white"))))
+							     :weight bold :foreground ,mf-default-fg))))
 
 			;; Notmuch
-			'(notmuch-search-date ((t (:foreground "white" :slant oblique :weight normal))))
-			'(notmuch-search-matching-authors ((t (:foreground "white" :weight semi-bold))))
-			'(notmuch-search-subject ((t (:foreground "white" :weight light))))
+			`(notmuch-search-date ((t (:foreground ,mf-default-fg :slant oblique :weight normal))))
+			`(notmuch-search-matching-authors ((t (:foreground ,mf-default-fg :weight semi-bold))))
+			`(notmuch-search-subject ((t (:foreground ,mf-default-fg :weight light))))
 			'(notmuch-search-unread-face ((t (:inherit bold :underline t))))
-			'(notmuch-tag-added ((t (:underline "white"))))
+			`(notmuch-tag-added ((t (:underline ,mf-default-fg))))
 			'(notmuch-tag-face ((t (:inherit shadow :foreground "#505050" :weight normal))))
-			'(notmuch-tag-unread ((t (:foreground "white" :weight bold))))
-			'(notmuch-tree-match-author-face ((t (:foreground "white" :weight semi-bold))))
-			'(notmuch-tree-match-tag-face ((t (:foreground "white" :weight bold))))
+			`(notmuch-tag-unread ((t (:foreground ,mf-default-fg :weight bold))))
+			`(notmuch-tree-match-author-face ((t (:foreground ,mf-default-fg :weight semi-bold))))
+			`(notmuch-tree-match-tag-face ((t (:foreground ,mf-default-fg :weight bold))))
 
 			;; Orderless
-			'(orderless-match-face-0 ((t (:foreground "white" :underline t  :weight bold))))
+			`(orderless-match-face-0 ((t (:foreground ,mf-default-fg :underline t  :weight bold))))
 			'(orderless-match-face-1 ((t (:inherit orderless-match-face-0))))
 			'(orderless-match-face-2 ((t (:inherit orderless-match-face-0))))
 			'(orderless-match-face-3 ((t (:inherit orderless-match-face-0))))
@@ -229,29 +241,32 @@
 
 			;; Org-mode
 			'(org-agenda-done ((t (:foreground "ForestGreen" :underline t :slant oblique :weight bold))))
-			'(org-agenda-structure ((t (:foreground "white"))))
-			'(org-agenda-dimmed-todo-face ((t (:foreground "white"))))
-			'(org-block ((t (:inherit shadow :extend t :background "#1e1e1e"))))
+			`(org-agenda-structure ((t (:foreground ,mf-default-fg))))
+			`(org-agenda-dimmed-todo-face ((t (:foreground ,mf-default-fg))))
+			`(org-block ((t (:inherit shadow :extend t :background ,mf-default-bg))))
 			'(org-date ((t (:underline t :foreground "#b0b0b0"))))
-			'(org-document-info ((t (:foreground "white"))))
-			'(org-document-title ((t (:foreground "white" :weight bold))))
+			`(org-document-info ((t (:foreground ,mf-default-fg))))
+			`(org-document-title ((t (:foreground ,mf-default-fg :weight bold))))
 			'(org-done ((t (:foreground "green" :underline t :slant oblique :weight bold))))
 			'(org-headline-done ((t (:extend t))))
 			'(org-level-1 ((t (:bold t :foreground "#bababa" :height 1.5))))
-			'(org-level-2 ((t (:extend nil :foreground "white" :weight normal :height 1.2))))
-			'(org-level-3 ((t (:extend nil :foreground "white" :weight bold :height 1.0))))
-			'(org-level-4 ((t (:extend nil :foreground "white" :weight normal :height 1.0))))
+			`(org-level-2 ((t (:extend nil :foreground ,mf-default-fg :weight normal :height 1.2))))
+			`(org-level-3 ((t (:extend nil :foreground ,mf-default-fg :weight bold :height 1.0))))
+			`(org-level-4 ((t (:extend nil :foreground ,mf-default-fg :weight normal :height 1.0))))
 			'(org-link ((t (:foreground "#b0b0b0" :underline t :slant oblique))))
-			'(org-scheduled ((t (:foreground "white" :slant oblique))))
-			'(org-table ((t (:foreground "white"))))
-			'(org-table-header ((t (:inherit org-table :foreground "white" :weight semi-bold))))
+			`(org-scheduled ((t (:foreground ,mf-default-fg :slant oblique))))
+			`(org-table ((t (:foreground ,mf-default-fg))))
+			`(org-table-header ((t (:inherit org-table :foreground ,mf-default-fg :weight semi-bold))))
 			'(org-time-grid ((t (:foreground "#a0a0a0"))))
-			'(org-todo ((t (:foreground "white" :underline t :slant oblique :weight bold))))
-			'(org-upcoming-deadline ((t (:foreground "white" :weight semi-bold))))
+			`(org-todo ((t (:foreground ,mf-default-fg :underline t :slant oblique :weight bold))))
+			`(org-upcoming-deadline ((t (:foreground ,mf-default-fg :weight semi-bold))))
 
 			;; Org-roam
 			'(org-roam-preview-heading ((t (:inherit org-block))))
 			'(org-roam-preview-heading-highlight ((t (:inherit org-roam-preview-heading))))
+
+			;; Perspective
+			'(persp-selected-face ((t (:weight bold))))
 
 			;; Show-paren
 			'(show-paren-match ((t (:background "#a0a0a0"))))
@@ -260,17 +275,17 @@
 			;; Speedbar
 			'(speedbar-button-face ((t (:foreground "#b3b3b3"))))
 			'(speedbar-file-face ((t (:foreground "#bababa"))))
-			'(speedbar-directory-face ((t (:weight bold :foreground "white"))))
+			`(speedbar-directory-face ((t (:weight bold :foreground ,mf-default-fg))))
 			'(speedbar-tag-face ((t (:foreground "#b3b3b3"))))
 			'(speedbar-selected-face ((t (:underline "#b3b3b3" :foreground "#bababa"))))
-			'(speedbar-highlight-face ((t (:weight bold :background "#1e1e1e" :foreground "white"))))
+			`(speedbar-highlight-face ((t (:weight bold :background ,mf-default-bg :foreground ,mf-default-fg))))
 
 			;; Vertico
-			'(vertico-current ((t (:underline "white" :background "#1e1e1e" :foreground "white"))))
+			`(vertico-current ((t (:background ,mf-medium-grey :foreground "black"))))
 
 			;; Whitespace-mode
 			'(whitespace-empty ((t (:background unspecified :foreground "red"))))
-			'(whitespace-line ((t (:background "#b3b3b3" :foreground "white"))))
+			`(whitespace-line ((t (:background "#b3b3b3" :foreground ,mf-default-fg))))
 			'(whitespace-trailing ((t (:background "red1")))))
 
 (provide-theme 'mf-dark)
