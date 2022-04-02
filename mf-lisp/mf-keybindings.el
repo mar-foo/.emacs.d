b6ab9a7772d66d1b2906508346111a743df467ba
Mario Forzanini <mario.forzainin@studenti.unimi.it>
2022-04-02 08:42:12 +0200

    mf-keybindings: Make leader a prefix map

--- mf-lisp/mf-keybindings.el
+++ mf-lisp/mf-keybindings.el
@@ -1,7 +1,12 @@
 ;; mf-keybindings.el --- Keybinding configuration -*- lexical-binding: t -*-
 	;;; Code:
+(defcustom mf/leader-prefix-key "C-c"
+  "Prefix key for my personal keybindings")
+
+(define-prefix-command 'mf/leader-map)
+
 (defmacro mf/leader (key func)
-  `(define-key global-map (kbd (concat "C-c " ,key)) #',func))
+  `(define-key'mf/leader-map (kbd ,key) #',func))
 
 (mf/leader "C-SPC" execute-extended-command)
 (mf/leader "d" dired)
@@ -12,10 +17,10 @@
 (mf/leader "k" kill-current-buffer)
 (mf/leader "m" mu4e)
 (mf/leader "p" bongo)
-(mf/leader "r" elfeed)
 (mf/leader "s" mf/toggle-shell)
+(mf/leader "S" mf/switch-theme)
 (mf/leader "t" mf/toggle-telega)
-(mf/leader "T" mf/switch-theme)
+(mf/leader "T" modus-themes-toggle)
 (mf/leader "w" eww)
 (mf/leader "y" mf/youtube)
 
@@ -26,6 +31,7 @@
 (mf/leader "n i" org-roam-node-insert)
 (mf/leader "n t" mf/org-roam-find-teaching)
 
+(global-set-key (kbd mf/leader-prefix-key) 'mf/leader-map)
 
 (define-key global-map (kbd "C-x C-b") (lambda () (interactive) (ibuffer t)))
 (define-key global-map (kbd "C-x M-f") #'mf/executable-find-file)
@@ -36,4 +42,4 @@
 (define-key lisp-mode-map (kbd "C-M-d") #'mf/delete-sexp)
 
 (provide 'mf-keybindings)
-	;;; mf-keybindings.el ends here
+;;; mf-keybindings.el ends here
